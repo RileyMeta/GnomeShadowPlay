7cdfe2412099a7095a15b91e67abe9c19b6a8d6b
Riley <32332593+RileyMeta@users.noreply.github.com>
2024-05-17 13:49:53 -0400

    Update main.py
    Moved `Gtk.main()` to the save_replay function.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from gi.repository import Gtk, Gdk, GLib, GdkPixbuf
 def save_replay():
     os.system('killall -SIGUSR1 gpu-screen-recorder')
     replay_notification.show_popup()
+    Gtk.main()
 
 if __name__ == "__main__":
     save_replay()
-    Gtk.main()
